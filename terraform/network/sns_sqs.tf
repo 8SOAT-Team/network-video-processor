9042150b4c20c233cd0d83938002c1d3a3d4a412
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2025-04-24 23:38:14 -0300

    Update sns_sqs.tf

--- terraform/network/sns_sqs.tf
+++ terraform/network/sns_sqs.tf
@@ -1,15 +1,25 @@
 resource "aws_sns_topic" "processor_topic" {
-  name = "processor-topic"
+  name                        = "processor-topic.fifo"
+  fifo_topic                  = true
+  content_based_deduplication = true
 }
 
 resource "aws_sqs_queue" "processor_queue" {
   name                        = "processor-queue.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = true
+}
+
+resource "aws_sqs_queue" "notification_queue" {
+  name                        = "notification-queue.fifo"
+  fifo_queue                  = true
+  content_based_deduplication = true
 }
 
 resource "aws_sns_topic_subscription" "processor_subscription" {
-  topic_arn = aws_sns_topic.processor_topic.arn
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.processor_queue.arn
+  topic_arn            = aws_sns_topic.processor_topic.arn
+  protocol             = "sqs"
+  endpoint             = aws_sqs_queue.processor_queue.arn
   raw_message_delivery = true
 }
 
@@ -31,3 +41,4 @@ resource "aws_sqs_queue_policy" "processor_policy" {
     }]
   })
 }
+

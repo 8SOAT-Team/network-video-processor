94fcff03ed99eaea578008a63c2b48e70cbd5f94
Darlei Randel <68856429+randeldarlei@users.noreply.github.com>
2025-04-25 00:19:26 -0300

    Update sns_sqs.tf

--- terraform/network/sns_sqs.tf
+++ terraform/network/sns_sqs.tf
@@ -1,11 +1,11 @@
-resource "aws_sns_topic" "processor_topic" {
-  name                        = "processor-topic.fifo"
+resource "aws_sns_topic" "video_start_processing" {
+  name                        = "video-start-processing.fifo"
   fifo_topic                  = true
   content_based_deduplication = true
 }
 
-resource "aws_sqs_queue" "processor_queue" {
-  name                        = "processor-queue.fifo"
+resource "aws_sqs_queue" "processing_video_consumer" {
+  name                        = "processing-video-consumer.fifo"
   fifo_queue                  = true
   content_based_deduplication = true
 }
@@ -16,15 +16,15 @@ resource "aws_sqs_queue" "notification_queue" {
   content_based_deduplication = true
 }
 
-resource "aws_sns_topic_subscription" "processor_subscription" {
-  topic_arn            = aws_sns_topic.processor_topic.arn
+resource "aws_sns_topic_subscription" "video_start_processing_subscription" {
+  topic_arn            = aws_sns_topic.video_start_processing.arn
   protocol             = "sqs"
-  endpoint             = aws_sqs_queue.processor_queue.arn
+  endpoint             = aws_sqs_queue.processing_video_consumer.arn
   raw_message_delivery = true
 }
 
-resource "aws_sqs_queue_policy" "processor_policy" {
-  queue_url = aws_sqs_queue.processor_queue.id
+resource "aws_sqs_queue_policy" "processing_video_consumer_policy" {
+  queue_url = aws_sqs_queue.processing_video_consumer.id
 
   policy = jsonencode({
     Version = "2012-10-17",
@@ -32,13 +32,12 @@ resource "aws_sqs_queue_policy" "processor_policy" {
       Effect    = "Allow",
       Principal = "*",
       Action    = "SQS:SendMessage",
-      Resource  = aws_sqs_queue.processor_queue.arn,
+      Resource  = aws_sqs_queue.processing_video_consumer.arn,
       Condition = {
         ArnEquals = {
-          "aws:SourceArn" = aws_sns_topic.processor_topic.arn
+          "aws:SourceArn" = aws_sns_topic.video_start_processing.arn
         }
       }
     }]
   })
 }
-
